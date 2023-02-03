b5ab217ea20271f3d93e0c44437caaa66ad74e72
Jeman1997 <mr.j06031997@gmail.com>
2023-02-03 14:26:12 +0530

    Feb 3 14:26

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question,Choice
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    ques_list=Question.objects.order_by('-pub_date')[:5]
+    return render(request,'polls/index.html',{'que_list':ques_list})
 def detail(request,question_id):
     return HttpResponse("You're looking at question {}".format(question_id))
 def results(request,questoin_id):
