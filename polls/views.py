f1c08cc56083d5f9e00ceb1e907e098b703fad40
Jeman1997 <mr.j06031997@gmail.com>
2023-02-03 13:21:49 +0530

    feb 3 2023 13:21

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World!')
+def detail(request,question_id):
+    return HttpResponse("You're looking at question {}".format(question_id))
+def results(request,questoin_id):
+    response="You're looking at the results of question {}".format(question_id)
+    return HttpResponse(response)
+def vote(request,question_id):
+    return HttpResponse("You're votin on question {}.".format(question_id))
