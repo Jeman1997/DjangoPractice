ee4fac3c801377de43f7937dde4f0e7ebefd0333
Jeman1997 <mr.j06031997@gmail.com>
2023-02-04 21:11:16 +0530

    feb 4 2023 21:11

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
 from .models import Question,Choice
 # Create your views here.
 def index(request):
     ques_list=Question.objects.order_by('-pub_date')[:5]
     return render(request,'polls/index.html',{'que_list':ques_list})
 def detail(request,question_id):
-    return HttpResponse("You're looking at question {}".format(question_id))
-def results(request,questoin_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
+def results(request,question_id):
     response="You're looking at the results of question {}".format(question_id)
     return HttpResponse(response)
 def vote(request,question_id):
